87c0cffc3423b372e5a645a2714909f5fb33b026
Vitalii <vyurkevich@ukr.net>
2017-11-06 00:45:57 +0200

    final

--- hometask6.php
+++ hometask6.php
@@ -132,7 +132,23 @@
 	echo implodius(" ", $array);
 	echo "<br>";
 	echo implode(" ", $array2);
-	echo "Завдання 8. Перетворити із масива у строку аналогічно implode <br>";
+	echo "<hr>";
+	echo "Завдання 8. Прибрати усі повторення символів зі строки. Вважатимемо, що регістр символів важливий<br>";
+	function delletrepeat($string) {
+		$arr = preg_split('//u', $string, null, PREG_SPLIT_NO_EMPTY);
+		$brr = array_count_values($arr);
+		$rslt = '';
+		foreach ($brr as $key => $value) {
+			$rslt = $rslt . $key;
+		}
+		return $rslt;
+	}
+	$txt = "аБвГґДеЄжЗАбВгҐаБвГґДеЄжЗАбВгҐ";
+	$txt2 = "Hello world!!";
+	echo "Дано масив: {$txt} <br>" . "Після застосування функції: " . delletrepeat($txt);
+	echo "<br>";
+	echo "Дано масив: {$txt2} <br>" . "Після застосування функції: " . delletrepeat($txt2) . '<br>';
+	echo "Принаймні працює так, як показано у прикладі.";
 ?>
 </body>
 </html>
